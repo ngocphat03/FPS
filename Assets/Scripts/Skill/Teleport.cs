10cbe6b59fd8fafee01d81d83587cc0804b273c1
ngocphat03 <ngonngocphat@gmail.com>
2022-10-23 02:03:54 +0700

    thêm hiệu ứng thở của player và hiệu ứng teleport

--- Assets/Scripts/Skill/Teleport.cs
+++ Assets/Scripts/Skill/Teleport.cs
@@ -1,14 +1,31 @@
 namespace Script.TeleportSkill
 {
     using UnityEngine;
+    using System.Collections;
     public class Teleport : MonoBehaviour
     {
-        private float distanceTeleport = 10f;
-        
+        [SerializeField] private GameObject Prefab;
+        private float distanceTeleport = 15f;
+        //<==== Timer====>
+        [SerializeField] private float timeStart = 0.5f;
+        private float lifeTimer;
+        private float timeStop = 0f;
+
+
+        private void Start()
+        {
+            SetTimer();
+        }
         private void Update()
         {
             TeleportLeft();
             TeleportRight();
+            TurnOnEffect();
+        }
+
+        private void FixedUpdate()
+        {
+            TurnOffEffect();
         }
 
         private void TeleportLeft()
@@ -18,6 +35,32 @@ namespace Script.TeleportSkill
         private void TeleportRight()
         {
             if(Input.GetKeyDown(KeyCode.E)) { transform.position += transform.right * distanceTeleport; }
+        }
+
+        private void SetTimer() { lifeTimer = timeStart; }
+
+        /// <summary>
+        /// Turn on teleport effect
+        /// </summary>
+        private void TurnOnEffect()
+        {
+            if(Input.GetKeyDown(KeyCode.Q) || Input.GetKeyDown(KeyCode.E)) 
+            { 
+                Prefab.SetActive(false);
+                Prefab.SetActive(true);
+            }
+        }
+
+        /// <summary>
+        /// Turn on teleport effect
+        /// </summary>
+        private void TurnOffEffect()
+        {
+            if(Prefab.active == true)
+            {
+                lifeTimer -= Time.deltaTime;
+                if(lifeTimer <= timeStop) { Prefab.SetActive(false);  SetTimer(); } 
+            }
         }
     }    
 }
